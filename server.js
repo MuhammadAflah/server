9fca7ac0566a082fd131ced21926ac33433e57e4
Muhammad Aflah <muhammadaflah786@gmail.com>
2023-06-17 18:45:11 +0530

    server updated

--- server.js
+++ server.js
@@ -1,54 +1,46 @@
 const express = require('express');
 const AWS = require('aws-sdk');
-const dotenv = require("dotenv");
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
-// AWS SDK Configuration
+const cloudFrontDomain = process.env.CLOUDFRONT_DOMAIN;
+const s3BucketName = process.env.BUCKET_NAME;
+const imageKey = process.env.S3_BUCKET_IMAGE_URL;
+const region = 'ap-northeast-1';
+
+// Configure AWS SDK with credentials
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'ap-northeast-1',
+  // signatureVersion: process.env.SIGNATURE_VERSION,
+  region: region,
 });
-
-// Create a new instance of the S3 service
-const s3 = new AWS.S3();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-app.get('/signed-url', (req, res) => {
-  const cloudFrontDomain = process.env.CLOUDFRONT_DOMAIN;
-  const imageKey = process.env.S3_BUCKET_IMAGE_URL;
+  const s3 = new AWS.S3();
 
-  // Calculate the expiration time
-  const expiresInSeconds = 300; // 5 minutes
-  const expirationTime = Math.floor(Date.now() / 1000) + expiresInSeconds;
-
+  // Generate a signed URL with a 5-minute expiry for the image file
   const params = {
-    Bucket: process.env.BUCKET_NAME,
+    Bucket: s3BucketName,
     Key: imageKey,
-    Expires: expiresInSeconds,
+    Expires: 120, // 5 minutes in seconds
   };
 
-  // Generate a signed URL
-  try {
-    const signedUrl = s3.getSignedUrl('getObject', params);
-    const cloudFrontSignedUrl = `https://${cloudFrontDomain}/${imageKey}?Expires=${expirationTime}&Signature=${encodeURIComponent(
-      signedUrl.split('Signature=')[1].split('&')[0]
-    )}&Key-Pair-Id=${process.env.CLOUDFRONT_KEY_PAIR_ID}`;
-
-    console.log(cloudFrontSignedUrl);
-    res.send(cloudFrontSignedUrl);
-  } catch (error) {
-    console.error('AWS SDK error:', error);
-    res.status(500).send('Internal Server Error');
-  }
+  s3.getSignedUrl('getObject', params, (err, url) => {
+    if (err) {
+      console.error('Error generating signed URL:', err);
+      res.sendStatus(500);
+    } else {
+      console.log('Signed URL:', url);
+      res.send(url)
+      return res.status(200).send("Check console for Signed URL"); //send message to see in console
+    }
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
